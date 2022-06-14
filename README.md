2b42162ac0336579f002bcfa396b7d4913e53023
willian-pessoa <willianpessoadeabreu@gmail.com>
2022-06-14 02:44:07 -0300

    first commit, fork the code from the lesson 9

---
+++ README.md
@@ -0,0 +1,5 @@
+# Cloth Store V2
+I'm doing this project code along following the course Complete React Developer in 2022 (w/ Redux, Hooks, GraphQL) by Zero to Mastery Academy.
+
+# Demo Live
+You can access the demo of this project by the link: 
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,76 @@
+import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from 'firebase/auth';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAzZ13LakbmniFn7hhqO7AKJifAz8auMwc",
+  authDomain: "cloth-store-db-v2.firebaseapp.com",
+  projectId: "cloth-store-db-v2",
+  storageBucket: "cloth-store-db-v2.appspot.com",
+  messagingSenderId: "1008567762748",
+  appId: "1:1008567762748:web:d159789ea25a1ddbada839"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const googleProvider = new GoogleAuthProvider();
+
+googleProvider.setCustomParameters({
+  prompt: 'select_account',
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () =>
+  signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, googleProvider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (
+  userAuth,
+  additionalInformation = {}
+) => {
+  if (!userAuth) return;
+
+  const userDocRef = doc(db, 'users', userAuth.uid);
+
+  const userSnapshot = await getDoc(userDocRef);
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+        ...additionalInformation,
+      });
+    } catch (error) {
+      console.log('error creating the user', error.message);
+    }
+  }
+
+  return userDocRef;
+};
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+
+  return await createUserWithEmailAndPassword(auth, email, password);
+};
+
+export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+
+  return await signInWithEmailAndPassword(auth, email, password);
+};
